5bcd6c2f776daf3a945e6986a8ab0601de562b1d
tobias <tobias.omondi@student.moringaschool.com>
2025-07-04 14:17:23 +0300

    Design Navbar Templ

--- school-system/src/Components/Navbar/Navbar.jsx
+++ school-system/src/Components/Navbar/Navbar.jsx
@@ -1,43 +1,46 @@
 import React from 'react';
-import logo from "/src/assets/schoollogo.webp"
-// import { Link } from "react-router-dom";
-
-// react-icons
+import logo from "/src/assets/schoollogo.webp";
 import { TfiMenuAlt } from "react-icons/tfi";
-
-// import css
-import './Navbar.css'
+import './Navbar.css';
+import NavbarSocial from './NavbarSocial';
 
 const Navbar = () => {
   return (
+    <>
+    <div className="Nav-bar-components">
 
-    // Parent Div
-    <div className='Nav-bar-components'>  
-      <div className='logo-top'>
-        <img src={logo} alt='School-Logo' />
+      {/* Logo */}
+      <div className="logo-top">
+        <img src={logo} alt="School-Logo" />
       </div>
 
-      {/* Name of the school title */}
-      <div className='school-name'>
-        <h1>BrightOak <br/> <span className='px-15'>Academy</span></h1>
+      {/* School Name */}
+      <div className="school-name">
+        <h1>
+          BrightOak <br />
+          <span className="px-15">Academy</span>
+        </h1>
       </div>
 
-      {/* links to navbar sytems */}
-
-      <div className='top-links'>
-        <ul className='flex flex-row  gap-20'>
+      {/* Top Links */}
+      <div className="top-links">
+        <ul className="flex flex-row gap-20">
           <h1>OUR SCHOOL</h1>
           <h1>ABOUT US</h1>
         </ul>
       </div>
 
-      {/* menu icon */}
-      <div className='menu-title-componets'>
-        <h1 >MENU</h1>
-        <h1>  <TfiMenuAlt /></h1>
+      {/* Menu */}
+      <div className="menu-title-componets">
+        <h1>MENU</h1>
+        <h1><TfiMenuAlt /></h1>
       </div>
+
     </div>
-  )
-}
+     <NavbarSocial title='FOLLOW US ON:' 
+      contact_title = 'CONTACT' email_title='EMAIL'/>
+    </>
+  );
+};
 
 export default Navbar;
---
+++ school-system/src/Components/Navbar/NavbarSocial.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Navbar.css'
+
+// socialmedia icons
+import { FaSquareInstagram, FaSquareYoutube, FaLinkedin } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+const NavbarSocial = ({title, contact_title, email_title}) => {
+  return (
+
+    <div className='Navbar-Social-Components'>
+
+
+      <div className='Navbar-Social-Title'>
+        <h3>{title}</h3>
+
+        <div className='Social-Media-Icons'>
+          <FaSquareInstagram />
+          <FaFacebookSquare />
+          <FaSquareYoutube />
+          <FaSquareXTwitter/>
+          <FaLinkedin />
+        </div>
+      </div>
+
+      <div className='Navbar-Contact'>
+        <h3>{contact_title}</h3>
+        <p>+254 756 798 298</p>
+        <p>+254 756 798 298</p>
+      </div>
+
+      <div className='Navbar-Email'>
+        <h3>{email_title}</h3>
+        <p>brightoakacademy@gmail.com</p>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default NavbarSocial
