1a755b830f0c8c3ec995751e6ef1c8bb7a4ec33f
tobias <tobias.omondi@student.moringaschool.com>
2025-07-16 11:56:25 +0300

    working on a herosection

--- school-system/src/Components/Navbar/HeroPage/Hero.jsx
+++ school-system/src/Components/Navbar/HeroPage/Hero.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import heroimage from '/src/assets/schoolhero.jpg'
-
-// import our css
-import './Hero.css'
+import heroimage from '/src/assets/schoolhero.jpg';
+import { CiSearch } from "react-icons/ci";
+import './Hero.css';
 
 const Hero = () => {
   return (
-    <div className='mt-5'>
+    <div className='flex flex-row justify-evenly items-center'>
+
       <div className='hero-container'>
-        <img src={heroimage} alt='school studenst'/>
+        <img src={heroimage} alt='school students' />
       </div>
+
+      {/* Search input with icon */}
+      <div className='relative mt-14 shadow-2xl p-10'>
+        <input 
+          type='search' 
+          id='search' 
+          placeholder='Search'
+          className='input-search'
+        />
+        <CiSearch className='absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-800 text-xl' />
+      </div>
+      
     </div>
-  )
+  );
 }
 
 export default Hero;
