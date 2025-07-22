ac0ecbb0786edd049556608eec86d0003088f194
tobias <tobias.omondi@student.moringaschool.com>
2025-07-22 22:51:34 +0300

    updated features

--- school-system/src/Components/HomePage/HomeCards.jsx
+++ school-system/src/Components/HomePage/HomeCards.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 // import css
 import './Home.css'
+import SliderHome from './SliderHome';
 
 const homeCards = [
   {id: 1, title: "PRE-PRIMARY", description: "For a pre-primary school like Bright Oak Academy, you'd want to highlight elements that parents are most interested in, such as nurturing environments, early childhood education, and developmental milestones. How are you thinking of framing it?"},
 
-  {id: 1, title: "PRIMARY SCHOOL & JUNIOR SCHOOL", description: "The Junior School is designed to guide learners through early adolescence—a time of rapid growth and self-discovery. At Bright Oak Academy, we provide a safe and supportive space where learners develop academic strength, social awareness, and personal values."}
+  {id: 2, title: "PRIMARY SCHOOL & JUNIOR SCHOOL", description: "The Junior School is designed to guide learners through early adolescence—a time of rapid growth and self-discovery. At Bright Oak Academy, we provide a safe and supportive space where learners develop academic strength, social awareness, and personal values."}
 ]
 
 const colors = ['#0077cc', '#253846'];
@@ -27,6 +28,8 @@ const HomeCards = () => {
           </div>
         )}
       </div>
+
+      {/* <> <SliderHome /></> */}
     </div>
   )
 }
--- school-system/src/Components/HomePage/SliderHome.jsx
+++ school-system/src/Components/HomePage/SliderHome.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
+
+// import swiper js components
+import Swiper from 'swiper'
+import { SwiperSlide, Swiper } from 'swiper/react'
+import { Pagination, Autoplay, EffectFade } from 'swiper/modules';
+
+// creating our data 
+
+const newsData = [
+  {id: 1, image: "", title: "", description: ""},
+  {id: 2, image: "", title: "", description: "" },
+  {id: 3, image: "", title: "", description: ""},
+  {id: 4, image: "", title: "", description: ""},
+]
+
 const SliderHome = () => {
   return (
-    <div>
-      
+    <div className='mt-20'>
+      <div>
+        <Swiper 
+          loop={true}
+          modules={[Pagination, Autoplay, EffectFade]}
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 6000 }}
+        >
+          <SwiperSlide></SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   )
 }
