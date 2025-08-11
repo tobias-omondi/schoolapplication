1fc6817fd32bce766c99c3c27e5957a3262db9fc
tobias <tobias.omondi@student.moringaschool.com>
2025-08-11 11:45:19 +0300

    added two files teachers and student

---
+++ school-system/src/Teachers/StudentLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentLogin = () => {
+  return (
+    <div>
+      <h1>Students Login</h1>
+    </div>
+  )
+}
+
+export default StudentLogin
---
+++ school-system/src/Teachers/TeachersLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeachersLogin = () => {
+  return (
+    <div>
+      <h1>Teachers Login</h1>
+    </div>
+  )
+}
+
+export default TeachersLogin
