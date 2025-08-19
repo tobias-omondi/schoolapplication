5316473627ad3126efe0bb9cc0e43fb2c5e003ae
tobias <tobias.omondi@student.moringaschool.com>
2025-08-19 12:01:58 +0300

    topdata live

---
+++ school-dashboard/src/components/NavbarComponents/TopNavbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TopNavbar = () => {
+  return (
+    <div>
+      {/* might be added if i need a sidebar
+       */}
+    </div>
+  )
+}
+
+export default TopNavbar
--- school-dashboard/src/components/OverviewComponents/Overview.jsx
+++ school-dashboard/src/components/OverviewComponents/Overview.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {motion} from 'framer-motion'
+import { LiaChalkboardTeacherSolid } from "react-icons/lia";
+import { PiStudent } from "react-icons/pi";
+import { SiGoogleclassroom } from "react-icons/si";
+
+const topData = [
+  {id: 1, icon: <LiaChalkboardTeacherSolid />, name: "Teachers"},
+  {id: 2, icon: <PiStudent />, name:" Students"},
+  {id: 3, icon: <SiGoogleclassroom />, name: "Classes"},
+]
 
 const Overview = () => {
   return (
     <div>
-      {/* <h1>Main page</h1> */}
+      <motion.div className='flex justify-evenly items-center gap-1 mx-auto py-5'>
+        {topData.map ((nav) =>
+        <motion.div key={nav.id} className='bg-white/60 border border-blue-500/30 backdrop-blur-lg py-10 px-36 rounded shadow-2xl'>
+          <h1 className='text-blue-500 text-4xl text-start'>{nav.icon}</h1>
+          <h6>{nav.name}</h6>
+        </motion.div>)}
+      </motion.div>
     </div>
   )
 }
