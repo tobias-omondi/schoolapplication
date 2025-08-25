3b2a552c4bd7e946e7cbd9cbc67dc82d2801c9d6
tobias <tobias.omondi@student.moringaschool.com>
2025-08-25 16:06:48 +0300

    Add Render domain to ALLOWED_HOSTS

--- backend/schoolbackend/settings.py
+++ backend/schoolbackend/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-+^p1hc*66*h6e+maxs+=&rz1ggbrf(_km4@km4v%&vl$_2hih8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+   'localhost',
+    '127.0.0.1',
+    'https://schoolapplication-6pbw.onrender.com/',
+]
 CORS_ALLOW_ALL_ORIGINS = True 
 
 
