063dbb7f45746886a02b1631399d7672e21c40b2
tobias <tobias.omondi@student.moringaschool.com>
2025-09-30 11:35:49 +0300

    a page for academic results

--- school-system/src/Components/AboutPage/AcademicResults.jsx
+++ school-system/src/Components/AboutPage/AcademicResults.jsx
@@ -26,10 +26,10 @@ const Counter = ({ target, duration, label }) => {
       transition={{ duration: 0.6 }}
       className="text-center"
     >
-      <h1 className="text-3xl font-semibold text-blue-600">
+      <h1 className="text-2xl font-semibold text-blue-600 underline underline-offset-4 decoration-red-950">
         {display}%
       </h1>
-      <p className="text-gray-700 text-lg">{label}</p>
+      <p className="text-gray-700 text-xl font-light">{label}</p>
     </motion.div>
   )
 }
@@ -45,30 +45,66 @@ const AcademicResults = () => {
         className='relative flex justify-center items-center mx-auto bg-blue-700 p-24'
       />
 
+
       {/* Title */}
       <div className='absolute top-48 left-10'>
         <h1 className='text-white text-3xl font-medium'>Academic Results</h1>
       </div>
 
+      <motion.div
+       initial = {{opacity:0, y:90}}
+       whileInView = {{opacity:1, y:0}}
+       transition = {{duration:0.8}}
+       viewport={{once:true}}
+       className='mt-10 text-center'>
+        <h1 className='text-gray-600 text-2xl'>Congratulation Class of 2025!</h1>
+      </motion.div>
+
+        <motion.div
+      initial = {{opacity:0, y:90}}
+      whileInView = {{opacity:1, y:0}}
+      transition = {{duration:0.8}}
+      viewport={{once:true}}
+       className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-10 px-5 text-center ">
+        <Counter target={30} duration={2} label="Achieved A*" />
+        <Counter target={55} duration={2.5} label="Achieved A*-A" />
+        <Counter target={45} duration={2} label="Achieved A*-B+" />
+        <Counter target={15} duration={1.8} label="Achieved A*-C" />
+      </motion.div>
+
       {/* Intro */}
-      <div className='mt-5 px-3'>
+      <motion.div className='mt-5 px-3'
+      initial = {{opacity:0, y:90}}
+      whileInView = {{opacity:1, y:0}}
+      transition = {{duration:0.8}}
+      viewport={{once:true}}>
         <h1 className='text-gray-700 font-medium text-2xl'>Academic Results & Performance</h1>
         <p className='text-gray-600 font-light'>
           At BrightOak Academy, academic excellence is at the core of our mission. Our results reflect both hard work <br /> 
           and commitment to holistic education.
         </p>
-      </div>
+      </motion.div>
 
       {/* Management Word */}
-      <div className='mt-5 px-3'>
+      <motion.div 
+      initial = {{opacity:0, y:90}}
+      whileInView = {{opacity:1, y:0}}
+      transition = {{duration:0.8}}
+      viewport={{once:true}}
+      className='mt-10 px-3'>
         <h1 className='text-gray-700 font-medium text-2xl'>A Word from School Management</h1>
         <p className='text-gray-600 font-light'>
           At BrightOak Academy, we take great pride in the consistent achievements of our students. Year after year, their results reflect not only academic excellence but also discipline, resilience, and the unwavering support of our dedicated teachers. We believe every child has unique potential, and these performances prove that with the right guidance, success is not just possible — it’s inevitable. As management, we remain committed to providing an environment where every learner thrives academically and personally.
         </p>
-      </div>
+      </motion.div>
 
       {/* Class Performance */}
-      <div className='mt-5 px-3'>
+      <motion.div 
+       initial = {{opacity:0, y:90}}
+      whileInView = {{opacity:1, y:0}}
+      transition = {{duration:0.8}}
+      viewport={{once:true}}
+      className='mt-5 px-3'>
         <h1 className='text-gray-700 font-medium text-2xl'>Class Performance – 2025</h1>
 
         <div className='flex flex-col md:flex-row lg:flex-row justify-evenly items-center gap-4 mt-5'>
@@ -82,19 +118,18 @@ const AcademicResults = () => {
             The <span className='text-blue-500 underline'>2025 class at BrightOak Academy</span> has set a remarkable standard of excellence. With a strong balance across sciences, arts, and humanities, our learners have showcased both academic discipline and creativity. This year’s results reflect the school’s steady growth and commitment to nurturing well-rounded achievers.
           </p>
         </div>
-      </div>
+      </motion.div>
 
       {/* Performance Counters */}
-      <div className='mt-10 text-center'>
-        <h1 className='text-gray-600 text-2xl'>Congratulation Class of 2025!</h1>
-      </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-10 px-5 text-center">
-        <Counter target={30} duration={2} label="/Achieved A*" />
-        <Counter target={55} duration={2.5} label="/Achieved A* - A" />
-        <Counter target={45} duration={2} label="/Achieved A* - B+" />
-        <Counter target={15} duration={1.8} label="/Achieved A* - C" />
-      </div>
-
+      <motion.div
+       initial = {{opacity:0, y:90}}
+       whileInView = {{opacity:1, y:0}}
+       transition = {{duration:0.8}}
+       viewport={{once:true}}
+       className='mt-10 text-center'>
+        <h1 className='text-gray-600 text-sm'>Congratulation Class of 2025!</h1>
+      </motion.div>
+  
       <div className='mt-10'>
         <Footer />
       </div>
