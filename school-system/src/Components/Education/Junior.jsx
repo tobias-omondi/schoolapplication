923f7cde99afc6619af135637a9682cde03f8f1f
tobias <tobias.omondi@student.moringaschool.com>
2025-09-30 11:40:30 +0300

    education pgs

---
+++ school-system/src/Components/Education/Junior.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Junior = () => {
+  return (
+    <div>
+      <h1>Junior School</h1>
+    </div>
+  )
+}
+
+export default Junior
---
+++ school-system/src/Components/Education/Primary.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Primary = () => {
+  return (
+    <div>
+      <h1>Primary</h1>
+    </div>
+  )
+}
+
+export default Primary
